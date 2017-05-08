bf933cd35b8b3a36c886fdf40a267ed66d95a7d1
malaffoon <laffoon@san.rr.com>
2017-05-08 14:05:42 -0700

    fix test

--- scripts/submit_xml.sh
+++ scripts/submit_xml.sh
@@ -5,15 +5,16 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-CREDS="-u dwtest@example.com:password"
-HOST="http://localhost:8080"
-
 pushd "$SCRIPT_DIR/../out"
+
+# fetch bearer token (need jq installed)
+HOST="http://localhost:8080"
+ACCESS_TOKEN=`curl -s -X POST --data "grant_type=password&username=dwtest@example.com&password=password&client_id=ets&client_secret=ets123" https://sso-deployment.sbtds.org:443/auth/oauth2/access_token?realm=/sbac | jq -r '.access_token'`
 
 # files are grouped by STATE/DISTRICT/SCHOOL
 for xml in */*/*/*.xml; do
     echo "submitting $xml"
-    curl -X POST ${CREDS} --header "Content-Type:application/xml" --data-binary @${xml} ${HOST}/exams/imports
+    curl -X POST --header "Authorization:Bearer ${ACCESS_TOKEN}" --header "Content-Type:application/xml" --data-binary @"${xml}" ${HOST}/exams/imports
 done
 
 popd
