cfde942a0f204d39804c63226a06f026afc75360
malaffoon <laffoon@san.rr.com>
2017-06-08 07:58:28 -0700

    changes to tabulator

--- data_generator/writers/tabulator_writer.py
+++ data_generator/writers/tabulator_writer.py
@@ -7,12 +7,13 @@ import csv
 TabulatorFieldNames = [
     'AssessmentId', 'AssessmentName', 'AssessmentSubject', 'AssessmentGrade', 'AssessmentType', 'AssessmentSubtype',
     'AssessmentLabel', 'AssessmentVersion', 'AcademicYear', 'FullItemKey', 'BankKey', 'ItemId', 'Filename', 'Version',
-    'ItemType', 'Grade', 'Standard', 'Claim', 'Target', 'OtherClaimTargets', 'CommonCore', 'PassageRef', 'ASL',
-    'Braille', 'LanguageBraille', 'DOK', 'Language', 'AllowCalculator', 'MathematicalPractice', 'MaxPoints',
-    'Glossary', 'ScoringEngine', 'Spanish', 'IsFieldTest', 'IsActive', 'ResponseRequired', 'AdminRequired',
-    'FormPosition', 'MeasurementModel', 'Weight', 'ScorePoints', 'a', 'b0_b', 'b1_c', 'b2', 'b3', 'avg_b', 'bpref1',
-    'bpref2', 'bpref3', 'bpref4', 'bpref5', 'bpref6', 'bpref7', 'CutPoint1', 'ScaledLow1', 'ScaledHigh1', 'CutPoint2',
-    'ScaledLow2', 'ScaledHigh2', 'CutPoint3', 'ScaledLow3', 'ScaledHigh3', 'CutPoint4', 'ScaledLow4', 'ScaledHigh4'
+    'ItemType', 'Grade', 'Standard', 'Claim', 'Target', 'PassageRef', 'ASL', 'Braille', 'LanguageBraille', 'DOK',
+    'Language', 'AllowCalculator', 'MathematicalPractice', 'MaxPoints', 'Glossary', 'ScoringEngine', 'Spanish',
+    'IsFieldTest', 'IsActive', 'ResponseRequired', 'AdminRequired', 'FormPosition', 'MeasurementModel', 'Weight',
+    'ScorePoints', 'a', 'b0_b', 'b1_c', 'b2', 'b3', 'avg_b', 'bpref1', 'bpref2', 'bpref3', 'bpref4', 'bpref5', 'bpref6',
+    'bpref7', 'CommonCore', 'ClaimContentTarget', 'SecondaryCommonCore', 'SecondaryClaimContentTarget', 'CutPoint1',
+    'ScaledLow1', 'ScaledHigh1', 'CutPoint2', 'ScaledLow2', 'ScaledHigh2', 'CutPoint3', 'ScaledLow3', 'ScaledHigh3',
+    'CutPoint4', 'ScaledLow4', 'ScaledHigh4'
 ]
 
 
