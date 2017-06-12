b63a7077061f9d4b7f8028e95ec5effd8fe1b344
malaffoon <laffoon@san.rr.com>
2017-06-12 09:04:42 -0700

    enhance script to reset access token periodically

--- scripts/submit_xml.sh
+++ scripts/submit_xml.sh
@@ -7,16 +7,23 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 pushd "$SCRIPT_DIR/../out"
 
-# fetch bearer token (need jq installed)
 HOST="http://localhost:8080"
-ACCESS_TOKEN=`curl -s -X POST --data "grant_type=password&username=dwtest@example.com&password=password&client_id=sbacdw&client_secret=sbacdw123" https://sso-deployment.sbtds.org:443/auth/oauth2/access_token?realm=/sbac | jq -r '.access_token'`
-# if host is using the "stub" token service, use this instead:
-#ACCESS_TOKEN="sbac;dwtest@example.com;|SBAC|ASMTDATALOAD|CLIENT|SBAC||||||||||||||"
 
 # files are grouped by STATE/DISTRICT/SCHOOL
+COUNT=1000000
 for xml in */*/*/*.xml; do
+    if [ ${COUNT} -gt 10000 ]; then
+        # fetch bearer token (need jq installed)
+        ACCESS_TOKEN=`curl -s -X POST --data 'grant_type=password&username=dwtest@example.com&password=password&client_id=sbacdw&client_secret=sbacdw123' 'https://sso-deployment.sbtds.org:443/auth/oauth2/access_token?realm=/sbac' | jq -r '.access_token' `
+        # if host is using the "stub" token service, use this instead:
+        #ACCESS_TOKEN="sbac;dwtest@example.com;|SBAC|ASMTDATALOAD|CLIENT|SBAC||||||||||||||"
+        echo "reset access token: ${ACCESS_TOKEN}"
+        COUNT=0
+    fi
+
     echo "submitting $xml"
     curl -X POST --header "Authorization:Bearer ${ACCESS_TOKEN}" -F file=@"${xml}" ${HOST}/exams/imports
+    COUNT=$((COUNT + 1))
 done
 
 popd
