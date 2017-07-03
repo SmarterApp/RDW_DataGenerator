298ad580e23a012971eaedd588be139212b21f39
malaffoon <laffoon@san.rr.com>
2017-07-03 16:53:51 -0700

    tweak submit_driver

--- scripts/submit_driver.sh
+++ scripts/submit_driver.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 # This scripts spawns multiple processes to more quickly submit xml files.
-# It is expecting to run for a single district, spawning processes for the schools one-at-a-time.
+# It should be run from the parent folder of 'out'.
+#
+# To submit all schools in a district folder, move to the district folder and replace outer loop:
+# for s in *; do
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-for s in *; do
+# files are grouped by STATE/DISTRICT/SCHOOL
+for s in ./out/CA/*/*; do
   echo "partitioning $s"
   for p in 0 1 2 3 4 5 6 7 8 9 a b c d e f ; do
-    ${SCRIPT_DIR}/submit_helper.sh $s $p &
+    ${SCRIPT_DIR}/submit_helper.sh ${s} ${p} &
   done
   wait
 done
