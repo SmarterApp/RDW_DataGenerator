f093f50eab5b8af67349fe1ae164943d10569e0a
Mark Laffoon <laffoon@san.rr.com>
2018-06-07 12:20:59 -0700

    tweaks to submit scripts to deal with change of file naming

--- scripts/submit_driver.sh
+++ scripts/submit_driver.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 # This scripts spawns multiple processes to more quickly submit xml files.
 # It should be run from the parent folder of 'out'.
-#
-# To submit all schools in a district folder, move to the district folder and replace outer loop:
-# for s in *; do
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# files are grouped by STATE/DISTRICT/SCHOOL
-for s in ./out/CA/*/*; do
-  echo "partitioning $s"
-  for p in 0 1 2 3 4 5 6 7 8 9 a b c d e f ; do
-    ${SCRIPT_DIR}/submit_helper.sh ${s} ${p} &
+# files are grouped by ./out/STATE/DISTRICT/SCHOOL
+# one DISTRICT at a time, each SCHOOL folder is given to a submit helper
+for d in ./out/*/*; do
+  echo "partitioning $d"
+  for f in ${d}/*; do
+    ${SCRIPT_DIR}/submit_helper.sh ${f} &
   done
   wait
 done
--- scripts/submit_xml.sh
+++ scripts/submit_xml.sh
@@ -8,13 +8,14 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd "$SCRIPT_DIR/../out"
 
 HOST="http://localhost:8080"
+#HOST="https://awsuat-import.sbacdw.org"
 
 # files are grouped by STATE/DISTRICT/SCHOOL
 COUNT=1000000
 for xml in */*/*/*.xml; do
     if [ ${COUNT} -gt 10000 ]; then
         # fetch bearer token (need jq installed)
-        #ACCESS_TOKEN=`curl -s -X POST --data 'grant_type=password&username=dwtest@example.com&password=password&client_id=sbacdw&client_secret=sbacdw123' 'https://sso-deployment.sbtds.org:443/auth/oauth2/access_token?realm=/sbac' | jq -r '.access_token' `
+        #ACCESS_TOKEN=`curl -s -X POST --data 'grant_type=password&username=username&password=password&client_id=sbacdw&client_secret=sbacdw123' 'https://sso-deployment.sbtds.org:443/auth/oauth2/access_token?realm=/sbac' | jq -r '.access_token' `
         # if host is using the "stub" token service, use this instead:
         ACCESS_TOKEN="sbac;dwtest@example.com;|SBAC|ASMTDATALOAD|CLIENT|SBAC||||||||||||||"
         echo "reset access token: ${ACCESS_TOKEN}"
